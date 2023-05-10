fc34192caac74ceab0e1225490648d38e1f2806f
vijay.nvb <vijay.nvb@gmail.com>
2023-05-10 09:13:33 +0530

    Rest API consumption in asp.net mvc project by creating a repository and using it

---
+++ TodoApp/Repository/RestApi/TodoRestRepository.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text;
+using TodoApp.Data;
+using TodoApp.Models;
+using TodoApp.ViewModels;
+
+namespace TodoApp.Repository.MsSQL
+{
+    public class TodoRestRepository : ITodoRepository
+    {
+        // interact with database and perform CRUD 
+
+        string baseURL = "https://jsonplaceholder.typicode.com/";
+        string baseURLLocal = "http://localhost:30010/api/v1";
+
+        HttpClient httpClient = new HttpClient();
+        
+        // http requestmessage 
+            // 5, host, version, header, body, verb [get, post, put, delete, trace ...]
+        // http responsemessage
+            // 4, status code, headers, body, version
+
+        public TodoRestRepository()
+        {
+        }
+
+        public Todo AddTodo(Todo newTodo)
+        {
+            // Todo id, TodoDTO without id mapp the data and make a request to rest api
+
+            TodoViewModel todoVM = new TodoViewModel();
+            todoVM.Title = newTodo.Title;
+            todoVM.Description = newTodo.Description;
+            todoVM.Status = newTodo.Status;
+            todoVM.DueDate = newTodo.DueDate;
+
+            string data = JsonConvert.SerializeObject(todoVM);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            var response = httpClient.PostAsync(baseURLLocal + "/todos", content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var responsecontent = response.Content.ReadAsStringAsync().Result;
+                Todo todo = JsonConvert.DeserializeObject<Todo>(responsecontent);
+                return todo;
+            }
+            return null;
+        }
+
+        public Todo DeleteTodo(int todoId)
+        {
+            var response = httpClient.DeleteAsync(baseURLLocal + "/todos/" + todoId).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;// json standard
+                Todo todo = JsonConvert.DeserializeObject<Todo>(data);
+                return todo;
+            }
+            return null;
+        }
+
+        public List<Todo> GetAllTodos()
+        {
+            // fetch todos from rest service -> http request message
+            // http://localhost:30010/api/v1/todos
+            var response = httpClient.GetAsync(baseURLLocal + "/todos").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;// json standard
+                // any json http response can be converted to a C# object by doing a
+                // serialization -> convert c# object to other data type [json, xml, paintext]
+                // deserialization -> convert other data type [json, xml, paintext] to C# object
+
+                // newtonsoft library which does serial deserial 
+                List<Todo> todos = JsonConvert.DeserializeObject<List<Todo>>(data);
+                return todos;
+            }
+            return null;
+        }
+
+        public Todo GetTodoById(int Id)
+        {
+            var response = httpClient.GetAsync(baseURLLocal + "/todos/"+Id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;// json standard
+                Todo todo = JsonConvert.DeserializeObject<Todo>(data);
+                return todo;
+            }
+            return null;
+        }
+
+        public Todo UpdateTodo(int todoId, Todo newTodo)
+        {
+            string data = JsonConvert.SerializeObject(newTodo);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            var response = httpClient.PutAsync(baseURLLocal + "/todos/"+todoId, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var responsecontent = response.Content.ReadAsStringAsync().Result;
+                Todo todo = JsonConvert.DeserializeObject<Todo>(responsecontent);
+                return todo;
+            }
+            return null;
+        }
+    }
+}
---
+++ TodoApp/ViewModels/TodoViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.ViewModels
+{
+    public class TodoViewModel
+    {
+        [Required(ErrorMessage = "Todo Titile is required")]
+        [MinLength(2)] // where title should be minimum of 2 characters
+        public string Title { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        public bool Status { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? DueDate { get; set; } // 
+
+        /*   [CustomAgeValidation(18)]
+           public int Age { get; set; } // age should be greater than 18
+   */
+        public TodoViewModel()
+        {
+        }
+        public TodoViewModel(string title, string description, bool status, DateTime dueDate)
+        {
+            Title = title;
+            Description = description;
+            Status = status;
+            DueDate = dueDate;
+        }
+    }
+}
