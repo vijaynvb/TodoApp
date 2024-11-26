db5bd8a3609eb70ac51f5afab5fcbfd7867b01b8
vijay.nvb <vijay.nvb@gmail.com>
2024-11-26 14:57:55 +0530

    Reports Screenshots

---
+++ NUnitSelenium1/NU_06_Reports.cs
@@ -0,0 +1,117 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.MarkupUtils;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Config;
+
+//using AventStack.ExtentReports.Reporter.Config;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+
+namespace NUnitSelenium1
+{
+    internal class NU_06_Reports
+    {
+        // install ExtentReports and ExtentReports.NUnit packages from nuget
+
+        private ExtentReports extent;
+        private ExtentTest test;
+
+        [OneTimeSetUp]
+        public void BeforeAllTest()
+        {
+            DateTime currentTime = DateTime.Now;
+            string fileName = "Extent_" + currentTime.ToString("yyyy-MM-dd_HH-mm-ss") + ".html";
+
+            //string fileName = "extent.html";
+            string pathtofile = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\reports\\" + fileName;
+
+            var htmlReporter = new ExtentSparkReporter(pathtofile);
+            htmlReporter.Config.Theme = Theme.Standard;
+            htmlReporter.Config.DocumentTitle = "CBA Test Suite";
+            htmlReporter.Config.ReportName = "Automation Test Results";
+            htmlReporter.Config.Encoding = "utf-8";
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+
+            extent.AddSystemInfo("Automation Tester", "Vijay Reddy");
+            extent.AddSystemInfo("Organization", "CBA");
+            extent.AddSystemInfo("Build No: ", "CBA-101");
+        }
+        #region :: Step 2 ::
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            //Get the test status to update in the report of each test
+            var testStatus = TestContext.CurrentContext.Result.Outcome.Status;
+            if (testStatus == TestStatus.Passed)
+            {
+                //test.Pass("Test case pass");
+                // more customizations can be done using below code with extent reports
+                IMarkup markup = MarkupHelper.CreateLabel("PASS", ExtentColor.Green);
+                test.Pass(markup);
+
+            }
+            else if (testStatus == TestStatus.Skipped)
+            {
+                test.Skip("Test Skipped");
+                //test.Skip("Test Skipped : " + TestContext.CurrentContext.Result.Message);
+                //IMarkup markup = MarkupHelper.CreateLabel("SKIP", ExtentColor.Amber);
+                //test.Skip(markup);
+            }
+            else if (testStatus == TestStatus.Failed)
+            {
+                test.Fail("Test Failed");
+                //test.Fail("Test Failed : " + TestContext.CurrentContext.Result.Message);
+                //IMarkup markup = MarkupHelper.CreateLabel("FAIL", ExtentColor.Red);
+                //test.Fail(markup);
+            }
+        }
+        #endregion
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            extent.Flush();
+        }
+
+        // different test cases
+        // one test case needed for generating the report
+
+        [Test]
+        public void LoginTest()
+        {
+
+            test.Info("Entering username");
+            test.Info("Entering password");
+            test.Info("Clicking on Submit button");
+            //test.Pass("Test case pass");
+            Assert.Pass("Test case pass");
+        }
+
+        [Test]
+        public void UserRegTest()
+        {
+            test.Info("Entering First Name");
+            test.Info("Entering Last Name");
+            // instead of fail , we can use Assert.Fail and
+            // capture the status in afftereachtest
+            // method to write in the report
+            // test.Fail("Failing the test case");
+            Assert.Fail("Failing the test case");
+        }
+
+        [Test]
+        public void ComposeEmailTest()
+        {
+            test.Info("Composing Email");
+           // test.Skip("Skipping the test case");
+            Assert.Ignore("Skipping the test case");
+        }
+    }
+}
---
+++ NUnitSelenium1/NU_09_CaptureScreenshot.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnitSelenium1
+{
+    internal class NU_09_CaptureScreenshot
+    {
+        static IWebDriver driver;
+
+        static void CaptureScreenshot()
+        {
+            DateTime currentTime = DateTime.Now;
+            string fileName = currentTime.ToString("yyyy-MM-dd_HH-mm-ss") + ".jpg";
+
+            Screenshot screenshot = ((WebDriver) driver).GetScreenshot();
+            screenshot.SaveAsFile(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent + "\\screenshot\\" + fileName);
+        }
+
+        static void CaptureElementScreenshot(IWebElement element)
+        {
+
+            DateTime currentTime = DateTime.Now;
+            string fileName = "Element_" + currentTime.ToString("yyyy-MM-dd_HH-mm-ss") + ".jpg";
+
+            Screenshot screenshot = ((ITakesScreenshot)element).GetScreenshot();
+            screenshot.SaveAsFile(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent + "\\screenshot\\" + fileName);
+        }
+
+        [Test]
+         public void TtyitPage()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.amazon.in/?tag=msndeskstdin-21&ref=pd_sl_1fp6radpzp_e&adgrpid=1326012680495996&hvadid=82876055419065&hvnetw=o&hvqmt=e&hvbmt=be&hvdev=c&hvlocint=&hvlocphy=156683&hvtargid=kwd-82876732654247:loc-90&hydadcr=5652_2377259";
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            CaptureScreenshot();
+
+           /* driver.SwitchTo().Frame("iframeResult");
+             driver.FindElement(By.XPath("/html/body/button")).Click();
+            IWebElement elem = driver.FindElement(By.Id("mySubmit"));
+            CaptureElementScreenshot(elem);*/
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Dispose();
+                driver.Quit();
+            }
+        }
+    }
+}
